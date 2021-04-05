b0c6dded3d9dd4f1ee7d8d7453eb72e961811209
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-05 19:12:52 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,3 +1,8 @@
+let state = {count: 0}
+let action = {type: 'INCREASE_COUNT'}
+
+changeState(state, action)
+
 function changeState(state, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
@@ -7,7 +12,18 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function render(){
+  document.body.textContent = state.count
+}
 
-changeState(state, action)
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
+
+render()
+
+//just for checking if everything is working correctly and state is show on the page after each change.
+dispatch({type: 'INCREASE_COUNT'})
+dispatch({type: 'INCREASE_COUNT'}) 
+
